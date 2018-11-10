a3e73de0bb6b933b8097c34220f9053897d913c2
yuki0508 <enla.festa.la.vita@icloud.com>
2018-11-10 16:00:13 +0900

    home

--- src/app/mock-taskcontents.ts
+++ src/app/mock-taskcontents.ts
@@ -2,5 +2,9 @@ import {TaskContent} from './task-content';
 
 export const TASKCONTENT : TaskContent[] =[
   {id:1,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
+  {id:2,picture:'syashin2',title:'Sentaku',user_id:'Arimura'},
+  {id:1,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
+  {id:2,picture:'syashin2',title:'Sentaku',user_id:'Arimura'},
+  {id:1,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
   {id:2,picture:'syashin2',title:'Sentaku',user_id:'Arimura'}
 ];
