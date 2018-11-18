5bd832a51ea8b19a5e91db727a8c734f5673df12
fumiya <challe.fumi@gmail.com>
2018-11-18 18:15:24 +0900

    曜日ごとのタスクの表示＆わかりやすいようにコメントを少し追加

--- src/app/mock-taskcontents.ts
+++ src/app/mock-taskcontents.ts
@@ -3,10 +3,10 @@ import {TaskContent} from './task-content';
 export const TASKCONTENT : TaskContent[] =[
   {id:1,day_of_week:0,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
   {id:2,day_of_week:0,picture:'syashin2',title:'Sentaku',user_id:'Arimura'},
-  {id:1,day_of_week:1,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
-  {id:2,day_of_week:1,picture:'syashin2',title:'Sentaku',user_id:'Arimura'},
-  {id:1,day_of_week:1,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
-  {id:2,day_of_week:1,picture:'syashin2',title:'Sentaku',user_id:'Arimura'},
-  {id:1,day_of_week:2,picture:'syashin',title:'Souzi',user_id:'Kobayashi'},
-  {id:2,day_of_week:3,picture:'syashin2',title:'Sentaku',user_id:'Arimura'}
+  {id:3,day_of_week:1,picture:'syashin3',title:'Souzi',user_id:'Kobayashi'},
+  {id:4,day_of_week:1,picture:'syashin4',title:'Sentaku',user_id:'Arimura'},
+  {id:5,day_of_week:1,picture:'syashin5',title:'Souzi',user_id:'Kobayashi'},
+  {id:6,day_of_week:1,picture:'syashin6',title:'Sentaku',user_id:'Arimura'},
+  {id:7,day_of_week:2,picture:'syashin7',title:'Souzi',user_id:'Kobayashi'},
+  {id:8,day_of_week:3,picture:'syashin8',title:'Sentaku',user_id:'Arimura'}
 ];
