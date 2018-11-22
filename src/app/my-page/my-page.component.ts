1704c42f69c21ee03a5972083aad76d038788bad
yuki0508 <enla.festa.la.vita@icloud.com>
2018-11-22 14:05:09 +0900

    pullするな

--- src/app/my-page/my-page.component.ts
+++ src/app/my-page/my-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../menu.service';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'ons-page[app-my-page]',
   templateUrl: './my-page.component.html',
   styleUrls: ['./my-page.component.scss']
 })
-export class MyPageComponent implements OnInit {
-
-  constructor() { }
+  export class MyPageComponent implements OnInit {
+  
+  home=HomeComponent;
+  constructor(private menuService: MenuService) { }
 
   ngOnInit() {
   }
-
+  load(component: any) {
+    this.menuService.loadComponent(component);
+  }
 }
