1e215b0e84c0625be3a97108794be7ff44a01a3b
fumiya <challe.fumi@gmail.com>
2018-11-30 18:57:08 +0900

    サイドバーの中身をngForを用いての表示（遷移がまだできてない！）

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Inject, forwardRef } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { MyPageComponent } from '../my-page/my-page.component';
-import {MenuService} from '../menu.service'
+import {MenuService} from '../menu.service';
+import { TASKCONTENT } from '../mock-taskcontents';
+
 @Component({
   selector: 'ons-page[app-menu]',
   templateUrl: './menu.component.html',
@@ -10,7 +12,7 @@ import {MenuService} from '../menu.service'
 export class MenuComponent implements OnInit {
 
   mypage=MyPageComponent;
-  
+  tasks=TASKCONTENT;
 
   constructor(private menuService: MenuService) { }
   // constructor(@Inject(forwardRef(() => AppComponent)) private app : AppComponent) { }
