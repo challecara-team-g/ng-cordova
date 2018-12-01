558bcf7f43647c384312dbf30e3a02d38ed94e6d
fumiya <challe.fumi@gmail.com>
2018-12-01 14:37:34 +0900

    作ってなかったデータの型作成

---
+++ src/app/group.ts
@@ -0,0 +1,11 @@
+export class group{
+    id:number;
+    name:string;
+    create_at:Date;
+    update_at:Date;
+    member_is:number;
+    join_at:Date;
+    task_id:number;
+    complete_at:Date;
+    assain_user_id:number;
+}
---
+++ src/app/users.ts
@@ -0,0 +1,13 @@
+export class Users{
+    id:string;
+    email:string;
+    phone_number:number;
+    name:string;
+    create_at:Date;
+    update_at:Date;
+    group_id:number;
+    joined_at:string;
+    task_id:number;
+    comment:string;
+    comment_at:string;
+}
