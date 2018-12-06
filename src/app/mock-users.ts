76855e0e7313c307d465c2bef5f13ce84fb7ec66
yuki0508 <enla.festa.la.vita@icloud.com>
2018-12-06 23:11:55 +0900

    img

--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -1,7 +1,7 @@
 import { Users } from './users';
 
 export const USERS : Users[] = [
-    {id:1,email:'',phone_number:111111,name:'チャレキャラ ',group_id:1,task_id:1,comment:'',comment_at:'',icon_id:''},
-    {id:2,email:'',phone_number:111111,name:'whark',group_id:2,task_id:2,comment:'',comment_at:'',icon_id:''},
+    {id:1,email:'',phone_number:111111,name:'チャレキャラ ',group_id:1,task_id:1,comment:'',comment_at:'',icon_id:'../assets/image/team1.png'},
+    {id:2,email:'',phone_number:111111,name:'whark',group_id:2,task_id:2,comment:'',comment_at:'',icon_id:'../assets/image/team2.png'},
 
 ]
